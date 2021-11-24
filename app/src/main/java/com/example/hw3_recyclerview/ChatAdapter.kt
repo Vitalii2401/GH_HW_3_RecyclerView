67f0b0d220f01fe7751b759a848c0f6c94f92bad
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-24 17:07:22 +0200

    Create ChatAdapter

---
+++ app/src/main/java/com/example/hw3_recyclerview/ChatAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.hw3_recyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+}
