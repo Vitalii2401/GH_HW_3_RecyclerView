5940c0ffbac69bdadb74222c665731e7732327a5
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-24 17:15:05 +0200

    Create ViewHolder

--- app/src/main/java/com/example/hw3_recyclerview/ChatAdapter.kt
+++ app/src/main/java/com/example/hw3_recyclerview/ChatAdapter.kt
@@ -2,6 +2,9 @@ package com.example.hw3_recyclerview
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hw3_recyclerview.databinding.HeaderBinding
+import com.example.hw3_recyclerview.databinding.LeftMessageBinding
+import com.example.hw3_recyclerview.databinding.RightMessageBinding
 
 class ChatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -14,4 +17,18 @@ class ChatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int {
     }
 
+
+
+
+    class ViewHolderLeftMessage(private val binding: LeftMessageBinding):
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    class ViewHolderRightMessage(private val binding: RightMessageBinding):
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    class ViewHolderHeader(private val binding: HeaderBinding):
+        RecyclerView.ViewHolder(binding.root) {
+    }
 }
