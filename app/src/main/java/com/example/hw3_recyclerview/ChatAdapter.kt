0f0e327125f7d734776356cf52aad6a9c4684c83
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-24 17:29:41 +0200

    Add adapter

--- app/src/main/java/com/example/hw3_recyclerview/ChatAdapter.kt
+++ app/src/main/java/com/example/hw3_recyclerview/ChatAdapter.kt
@@ -1,20 +1,68 @@
 package com.example.hw3_recyclerview
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hw3_recyclerview.databinding.HeaderBinding
 import com.example.hw3_recyclerview.databinding.LeftMessageBinding
 import com.example.hw3_recyclerview.databinding.RightMessageBinding
+import java.util.ArrayList
 
 class ChatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    var messages: ArrayList<Messages> = arrayListOf(Messages(0, "0"))
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            1 -> {
+                val binding = LeftMessageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderLeftMessage(binding)
+            }
+            2 -> {
+                val binding = RightMessageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderRightMessage(binding)
+            }
+            else -> {
+                val binding = HeaderBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderHeader(binding)
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is ViewHolderLeftMessage -> holder.bind(messages[position])
+            is ViewHolderRightMessage -> holder.bind(messages[position])
+            is ViewHolderHeader -> holder.bind()
+        }
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount() = messages.size
+
+    override fun getItemViewType(position: Int): Int {
+        return when (messages[position].userId) {
+            1 -> VIEW_TYPE_MESSAGE_LEFT
+            2 -> VIEW_TYPE_MESSAGE_RIGHT
+            else -> {CHAT_HEADER}
+        }
+    }
+
+    companion object {
+        const val CHAT_HEADER = 0
+        const val VIEW_TYPE_MESSAGE_LEFT = 1
+        const val VIEW_TYPE_MESSAGE_RIGHT = 2
     }
 
 
@@ -22,13 +70,23 @@ class ChatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class ViewHolderLeftMessage(private val binding: LeftMessageBinding):
         RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: Messages){
+            binding.leftMessageTextView.text = user.message
+        }
     }
 
     class ViewHolderRightMessage(private val binding: RightMessageBinding):
         RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: Messages){
+            binding.rightMessageTextView.text = user.message
+        }
     }
 
     class ViewHolderHeader(private val binding: HeaderBinding):
         RecyclerView.ViewHolder(binding.root) {
+        fun bind() = with(binding) {
+            userFirstButton.text = "User 1: "
+            userSecondButton.text = "User 2: "
+        }
     }
 }
--- app/src/main/java/com/example/hw3_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/hw3_recyclerview/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.hw3_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hw3_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = ChatAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter(){
+        binding.apply {
+            chatRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            chatRecyclerView.adapter = adapter
+        }
     }
 }
